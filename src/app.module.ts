1db4ed626930bfbba4a98ee0baaf09e67db436e2
Lotfi Anwar L Arif <52082662+Lotfi-Arif@users.noreply.github.com>
2021-07-12 22:49:33 +0800

    Fix db password problem

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { Banner } from './banners/entities/banner.entity';
       host: 'db',
       port: 3306,
       username: 'root',
-      password: 'password',
+      password: 'Web.Tech123',
       database: 'dev_profile',
       autoLoadEntities: true,
       entities: [Banner],
